4c519989d72f977a3cf8cfc8db9eb5936a11328c
yafira <ym3317@nyu.edu>
2025-01-18 23:39:36 -0500

    added icons to boxes

--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,58 @@ export default function HomePage() {
 		<div className='container'>
 			<div className='image-box' style={{ position: 'relative' }}>
 				<Image
-					src='/flower.png'
+					src='/assets/flower.png'
 					alt='flower shape'
 					className='flower-image'
 					width={500}
 					height={500}
 				/>
 				{/* smaller boxes */}
-				<div className='line-box box1'></div>
-				<div className='line-box box2'></div>
-				<div className='line-box box3'></div>
-				<div className='line-box box4'></div>
-				<div className='line-box box5'></div>
+				<div className='line-box box1'>
+					<Image
+						src='/assets/tools.png'
+						alt='craft icon'
+						width={60}
+						height={60}
+					/>
+					<span className='box-text'>craft</span>
+				</div>
+				<div className='line-box box2'>
+					<Image
+						src='/assets/code.png'
+						alt='code icon'
+						width={60}
+						height={60}
+					/>
+					<span className='box-text'>code</span>
+				</div>
+				<div className='line-box box3'>
+					<Image
+						src='/assets/design.png'
+						alt='design icon'
+						width={60}
+						height={60}
+					/>
+					<span className='box-text'>design</span>
+				</div>
+				<div className='line-box box4'>
+					<Image
+						src='/assets/circuit.png'
+						alt='electronics icon'
+						width={60}
+						height={60}
+					/>
+					<span className='box-text'>electronics</span>
+				</div>
+				<div className='line-box box5'>
+					<Image
+						src='/assets/text.png'
+						alt='text icon'
+						width={60}
+						height={60}
+					/>
+					<span className='box-text'>text</span>
+				</div>
 			</div>
 		</div>
 	)
