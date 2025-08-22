58ba42c16a00c827d273d684a8b29b33ed0a9f4f
Yafira <ym3317@nyu.edu>
2025-08-22 17:01:51 -0400

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,21 @@ const pixelifySans = Pixelify_Sans({
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
+			<link
+				rel='icon'
+				type='image/png'
+				href='/favicon-96x96.png'
+				sizes='96x96'
+			/>
+			<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+			<link rel='shortcut icon' href='/favicon.ico' />
+			<link
+				rel='apple-touch-icon'
+				sizes='180x180'
+				href='/apple-touch-icon.png'
+			/>
+			<meta name='apple-mobile-web-app-title' content='my-website' />
+			<link rel='manifest' href='/site.webmanifest' />
 			<body className={pixelifySans.className}>
 				<nav className='navigation'>
 					<div className='nav-links'>
