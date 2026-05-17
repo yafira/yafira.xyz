555418d9ff3c719d6418bc5a5ba6a4756ec32260
Yafira <ym3317@nyu.edu>
2026-05-17 16:33:00 -0400

    updated metadata

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import MobileBack from "@/app/components/MobileBack";
 
 export const metadata = {
-  title: "yafira.xyz",
+  title: "Yafira ✿",
 };
 
 /* eslint-disable react/no-unescaped-entities */
