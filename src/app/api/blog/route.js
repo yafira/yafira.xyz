6804d9f8ec30fc9ccd99e1f2512ee8f6b3bf432b
electrocute <ym3317@nyu.edu>
2026-07-07 21:14:47 -0400

    updated blog site host url

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -3,7 +3,7 @@ export const revalidate = 300;
 export const runtime = 'nodejs';
 
 const SITES = [
-	{ host: 'electrocuteblog.wordpress.com', label: 'electrocute' },
+	{ host: 'https://blog.electrocute.io/', label: 'electrocute' },
 	{ host: 'electrocuteitp.wordpress.com', label: 'itp' },
 ];
 
